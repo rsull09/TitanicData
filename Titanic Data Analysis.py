8c9b29ecca52a6cb2934d7e4a23c4ebba4fe00af
rsull09 <ray.sullivan94@gmail.com>
2017-11-28 16:27:55 -0600

    Update Titanic Data Analysis.py

--- Titanic Data Analysis.py
+++ Titanic Data Analysis.py
@@ -1,9 +1,4 @@
 
-# coding: utf-8
-
-# In[8]:
-
-
 #data analysis
 import pandas as pd
 import numpy as np
@@ -24,71 +19,37 @@ from sklearn.linear_model import Perceptron
 from sklearn.linear_model import SGDClassifier
 from sklearn.tree import DecisionTreeClassifier
 
-
-# In[9]:
-
-
 #import data
 train_df = pd.read_csv("C:\python\\titanic_train.csv")
 test_df = pd.read_csv("C:\python\\titanic_test.csv")
 combine = [train_df, test_df]
-
-
-# In[4]:
-
 
 #Preview of the data
 print(train_df.columns.values)
 train_df.head()
 train_df.tail()
 
-
-# In[5]:
-
-
 #more info
 train_df.info()
 print('_'*40)
 test_df.info()
 
-
-# In[6]:
-
-
 train_df[['Pclass','Survived']].groupby(['Pclass'], 
                 as_index=False).mean().sort_values(by='Survived', ascending=False)
-
-
-# In[7]:
-
 
 train_df[['Sex','Survived']].groupby(['Sex'], 
                 as_index=False).mean().sort_values(by='Survived', ascending=False)
 
-
-# In[10]:
-
-
 train_df[['SibSp','Survived']].groupby(['SibSp'], 
                 as_index=False).mean().sort_values(by='Survived', ascending=False)
-
-
-# In[11]:
-
 
 train_df[['Parch','Survived']].groupby(['Parch'], 
                 as_index=False).mean().sort_values(by='Survived', ascending=False)
 
 
-# In[12]:
-
-
 #plotting data
 g = sns.FacetGrid(train_df, col='Survived')
 g.map(plt.hist, 'Age', bins=20)
-
-
-# In[13]:
 
 
 #grid plot
@@ -97,23 +58,15 @@ grid.map(plt.hist, 'Age', alpha=.5, bins=20)
 grid.add_legend();
 
 
-# In[14]:
-
-
 grid = sns.FacetGrid(train_df, row='Embarked', size=2.2, aspect=1.6)
 grid.map(sns.pointplot, 'Pclass', 'Survived', 'Sex', palette='deep')
 grid.add_legend()
 
 
-# In[15]:
-
 
 grid = sns.FacetGrid(train_df, row='Embarked', col='Survived', size=2.2, aspect=1.6)
 grid.map(sns.barplot, 'Sex', 'Fare', alpha=.5, ci=None)
 grid.add_legend()
-
-
-# In[16]:
 
 
 print("Before", train_df.shape, test_df.shape, combine[0].shape, combine[1].shape)
@@ -125,18 +78,11 @@ combine = [train_df, test_df]
 "After", train_df.shape, test_df.shape, combine[0].shape, combine[1].shape
 
 
-# In[17]:
-
-
 #Working with names
 for dataset in combine:
     dataset['Title'] = dataset.Name.str.extract('([A-Za-z]+)\.',
                                 expand=False)
 pd.crosstab(train_df['Title'], train_df['Sex'])
-
-
-# In[18]:
-
 
 for dataset in combine:
     dataset['Title'] = dataset['Title'].replace(['Lady', 'Countess','Capt', 'Col',     'Don', 'Dr', 'Major', 'Rev', 'Sir', 'Jonkheer', 'Dona'], 'Rare')
@@ -148,9 +94,6 @@ for dataset in combine:
 train_df[['Title', 'Survived']].groupby(['Title'], as_index=False).mean()
 
 
-# In[19]:
-
-
 title_mapping = {"Mr.": 1, "Miss": 2, "Mrs.": 3, "Master": 4,
                  "Rare": 5}
 for dataset in combine:
@@ -160,16 +103,10 @@ for dataset in combine:
 train_df.head()
 
 
-# In[20]:
-
-
 train_df = train_df.drop(['Name', 'PassengerId'], axis=1)
 test_df = test_df.drop(['Name'], axis=1)
 combine = [train_df, test_df]
 train_df.shape, test_df.shape
-
-
-# In[25]:
 
 
 #Changing sex category to numerical
@@ -180,24 +117,15 @@ for dataset in combine:
 train_df.head()
 
 
-# In[26]:
-
-
 grid = sns.FacetGrid(train_df, row='Pclass', col='Sex',
                     size=2.2, aspect=1.6)
 grid.map(plt.hist, 'Age', alpha=.5, bins=20)
 grid.add_legend()
 
 
-# In[29]:
-
-
 #Guessing ages
 guess_ages = np.zeros((2,3))
 guess_ages
-
-
-# In[36]:
 
 
 for dataset in combine:
@@ -223,14 +151,8 @@ for dataset in combine:
 train_df.head()
 
 
-# In[37]:
-
-
 train_df['AgeBand'] = pd.cut(train_df['Age'], 5)
 train_df[['AgeBand', 'Survived']].groupby(['AgeBand'], as_index=False).mean().sort_values(by='AgeBand', ascending=True)
-
-
-# In[38]:
 
 
 for dataset in combine:    
@@ -241,17 +163,9 @@ for dataset in combine:
     dataset.loc[ dataset['Age'] > 64, 'Age']
 train_df.head()
 
-
-# In[39]:
-
-
 train_df = train_df.drop(['AgeBand'], axis=1)
 combine = [train_df, test_df]
 train_df.head()
-
-
-# In[40]:
-
 
 #Creating new features by combining existing features
 #Create new features for Family size so that we can drop parch and sibsp
@@ -262,19 +176,11 @@ for dataset in combine:
 train_df[['FamilySize', 'Survived']].groupby(['FamilySize'],
                                             as_index=False).mean().sort_values(by='Survived', ascending=False)
 
-
-# In[42]:
-
-
 for dataset in combine:
     dataset['IsAlone'] = 0
     dataset.loc[dataset["FamilySize"] == 1, 'IsAlone'] = 1
 
 train_df[['IsAlone', 'Survived']].groupby(['IsAlone'], as_index=False).mean()
-
-
-# In[47]:
-
 
 train_df = train_df.drop(['Parch', 'SibSp', 'FamilySize'], axis=1)
 test_df = test_df.drop(['Parch', 'SibSp', 'FamilySize'], axis=1)
